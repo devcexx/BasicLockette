c02fb1b2d474e901dfb114eda3b7ab00215d6ca2
Roberto Guillén <robertoguillen@me.com>
2024-06-02 01:58:28 +0200

    Fixed version tag

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "me.devcexx"
-version = "1.0-SNAPSHOT"
+version = "0.1-SNAPSHOT"
 
 val testServerPath = projectDir.resolve("server")
 val testServerPluginsPath = testServerPath.resolve("plugins")
